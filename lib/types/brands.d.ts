8d4b47b5b4ac9f2bfebccf3992b773b70f0aeeff
Jigar Dafda <jigar.dafda@gmail.com>
2024-06-21 04:36:43 +0000

    [Auto Generated]

--- lib/types/brands.d.ts
+++ lib/types/brands.d.ts
@@ -2,4 +2,5 @@ import { CommonAsyncLoadingTypes } from ".";
 export type BrandsType = Partial<any & CommonAsyncLoadingTypes>;
 export type BrandsSlice = {
     brands: BrandsType;
+    brand: any;
 };
