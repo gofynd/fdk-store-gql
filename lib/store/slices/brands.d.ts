e1d7ab03bae9ec1f77e5cd6f91a9533c0404a05a
Jigar Dafda <jigar.dafda@gmail.com>
2024-06-28 10:01:36 +0000

    [Auto Generated]

--- lib/store/slices/brands.d.ts
+++ lib/store/slices/brands.d.ts
@@ -12,4 +12,16 @@ export declare const fetchBrands: import("@reduxjs/toolkit").AsyncThunk<any, any
     fulfilledMeta?: unknown;
     rejectedMeta?: unknown;
 }>;
+export declare const fetchBrand: import("@reduxjs/toolkit").AsyncThunk<any, any, {
+    extra: {
+        gqClient: import("@apollo/client").ApolloClient<import("@apollo/client").NormalizedCacheObject>;
+    };
+    rejectValue: SerializedError;
+    state?: unknown;
+    dispatch?: import("redux").Dispatch<import("redux").AnyAction> | undefined;
+    serializedErrorType?: unknown;
+    pendingMeta?: unknown;
+    fulfilledMeta?: unknown;
+    rejectedMeta?: unknown;
+}>;
 export declare const brandsReducer: import("redux").Reducer<BrandsSlice, import("redux").AnyAction>;
