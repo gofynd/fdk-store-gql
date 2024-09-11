aa3f131190e413cde9618325104cde5fc6328972
Jigar Dafda <jigar.dafda@gmail.com>
2024-09-11 12:01:55 +0000

    [Auto Generated]

--- lib/store/slices/theme.d.ts
+++ lib/store/slices/theme.d.ts
@@ -1,43 +1,7 @@
-import { SerializedError } from "@reduxjs/toolkit";
 import { CustomAction } from "../../types";
 import { AddSectionEvent, DragSectionEvent, RemoveSection, ThemeSlice, UpdateSection } from "../../types/theme";
 import { STORE_KEYS } from "../enums/keys";
-export declare const fetchAppliedTheme: import("@reduxjs/toolkit").AsyncThunk<any, any, {
-    extra: {
-        gqClient: import("@apollo/client").ApolloClient<import("@apollo/client").NormalizedCacheObject>;
-    };
-    rejectValue: SerializedError;
-    state?: unknown;
-    dispatch?: import("redux").Dispatch<import("redux").AnyAction> | undefined;
-    serializedErrorType?: unknown;
-    pendingMeta?: unknown;
-    fulfilledMeta?: unknown;
-    rejectedMeta?: unknown;
-}>;
-export declare const fetchThemeById: import("@reduxjs/toolkit").AsyncThunk<any, any, {
-    extra: {
-        gqClient: import("@apollo/client").ApolloClient<import("@apollo/client").NormalizedCacheObject>;
-    };
-    rejectValue: SerializedError;
-    state?: unknown;
-    dispatch?: import("redux").Dispatch<import("redux").AnyAction> | undefined;
-    serializedErrorType?: unknown;
-    pendingMeta?: unknown;
-    fulfilledMeta?: unknown;
-    rejectedMeta?: unknown;
-}>;
-export declare const fetchPage: import("@reduxjs/toolkit").AsyncThunk<any, any, {
-    extra: {
-        gqClient: import("@apollo/client").ApolloClient<import("@apollo/client").NormalizedCacheObject>;
-    };
-    rejectValue: SerializedError;
-    state?: unknown;
-    dispatch?: import("redux").Dispatch<import("redux").AnyAction> | undefined;
-    serializedErrorType?: unknown;
-    pendingMeta?: unknown;
-    fulfilledMeta?: unknown;
-    rejectedMeta?: unknown;
-}>;
+import { Maybe, PageDetail, ThemeDetail } from "../../types/graphql";
 declare const themeSlice: import("@reduxjs/toolkit").Slice<ThemeSlice, {
     dragSection(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<DragSectionEvent>): void;
     addSection(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<AddSectionEvent>): void;
@@ -51,7 +15,11 @@ declare const themeSlice: import("@reduxjs/toolkit").Slice<ThemeSlice, {
         payload: any;
         type: string;
     }): void;
+    setAppliedTheme(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<ThemeDetail>): void;
+    setPreviewTheme(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<ThemeDetail>): void;
+    setthemePageDetail(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<PageDetail>): void;
+    setthemePages(state: import("immer/dist/internal").WritableDraft<ThemeSlice>, action: CustomAction<Array<Maybe<PageDetail>>>): void;
 }, STORE_KEYS.THEME>;
 export declare const themeReducer: import("redux").Reducer<ThemeSlice, import("redux").AnyAction>;
-export declare const dragSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<DragSectionEvent, "theme/dragSection">, addSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<AddSectionEvent, "theme/addSection">, removeSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<RemoveSection, "theme/removeSection">, updateSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<UpdateSection, "theme/updateSection">, setTheme: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "theme/setTheme">, setGlobalConfig: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "theme/setGlobalConfig">;
+export declare const dragSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<DragSectionEvent, "theme/dragSection">, addSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<AddSectionEvent, "theme/addSection">, removeSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<RemoveSection, "theme/removeSection">, updateSection: import("@reduxjs/toolkit").ActionCreatorWithPayload<UpdateSection, "theme/updateSection">, setTheme: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "theme/setTheme">, setGlobalConfig: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "theme/setGlobalConfig">, setAppliedTheme: import("@reduxjs/toolkit").ActionCreatorWithPayload<ThemeDetail, "theme/setAppliedTheme">, setPreviewTheme: import("@reduxjs/toolkit").ActionCreatorWithPayload<ThemeDetail, "theme/setPreviewTheme">, setthemePageDetail: import("@reduxjs/toolkit").ActionCreatorWithPayload<PageDetail, "theme/setthemePageDetail">, setthemePages: import("@reduxjs/toolkit").ActionCreatorWithPayload<Maybe<PageDetail>[], "theme/setthemePages">;
 export default themeSlice;
