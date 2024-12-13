d6bd058723c5e29fab1b69d8a64b3f456b82871f
Jigar Dafda <jigar.dafda@gmail.com>
2024-12-13 10:20:44 +0000

    [Auto Generated]

---
+++ lib/graphql/query/followedListing.handler.d.ts
@@ -0,0 +1,3 @@
+import type { ThunkActionDispatch } from "redux-thunk";
+import { FollowListing } from "../../types/graphql";
+export declare function followedListingHandler(followedListing: FollowListing, dispatch: ThunkActionDispatch<any>): void;
