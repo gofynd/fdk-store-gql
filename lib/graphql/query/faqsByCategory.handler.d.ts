d50a004c321d4e6430105bea2f102ee546fdff77
Jigar Dafda <jigar.dafda@gmail.com>
2024-12-17 10:35:32 +0000

    [Auto Generated]

---
+++ lib/graphql/query/faqsByCategory.handler.d.ts
@@ -0,0 +1,3 @@
+import type { ThunkActionDispatch } from "redux-thunk";
+import { FaQs } from "../../types/graphql";
+export declare function faqsByCategoryHandler(faqsByCategory: FaQs, dispatch: ThunkActionDispatch<any>): void;
--- lib/store/slices/categories.d.ts
+++ lib/store/slices/categories.d.ts
@@ -1,14 +1,4 @@
 import { CategoriesSlice } from "../../types/categories";
-export declare const fetchCategories: import("@reduxjs/toolkit").AsyncThunk<any, any, {
-    extra: {
-        gqClient: import("@apollo/client").ApolloClient<import("@apollo/client").NormalizedCacheObject>;
-    };
-    rejectValue: import("@reduxjs/toolkit").SerializedError;
-    state?: unknown;
-    dispatch?: import("redux").Dispatch<import("redux").AnyAction> | undefined;
-    serializedErrorType?: unknown;
-    pendingMeta?: unknown;
-    fulfilledMeta?: unknown;
-    rejectedMeta?: unknown;
-}>;
+import { Categories } from "../../types/graphql";
 export declare const categoriesReducer: import("redux").Reducer<CategoriesSlice, import("redux").AnyAction>;
+export declare const setCategories: import("@reduxjs/toolkit").ActionCreatorWithPayload<Categories, "categories/setCategories">;
