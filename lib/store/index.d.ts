c4c27d0857fe98d491d548350c6e813fabf12682
Jigar Dafda <jigar.dafda@gmail.com>
2024-12-23 08:50:16 +0000

    [Auto Generated]

--- lib/store/index.d.ts
+++ lib/store/index.d.ts
@@ -15,8 +15,11 @@ declare class ApplicationStore {
     PaymentGateways: Record<string, any>;
     private domain;
     private authorizationHeader;
+    private serverCookies;
     constructor(initialData: any, authorizationHeader: any, domain: any);
     private executeFetch;
+    private getServerCookies;
+    setServerCookies(cookies?: Record<string, string>): void;
     setI18nDetails(cookieValue: any): void;
     observeStore(getterKey: string, onChange: Function): import("redux").Unsubscribe;
     executeGQL(query: string, params: any | null, options?: ExecuteOptions): Promise<any>;
