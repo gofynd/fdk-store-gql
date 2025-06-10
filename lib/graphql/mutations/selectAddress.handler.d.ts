1df1533474cfbbc29f6738356a9719a4d38909ba
Jigar Dafda <jigar.dafda@gmail.com>
2025-06-10 14:56:09 +0000

    [Auto Generated]

--- lib/graphql/mutations/selectAddress.handler.d.ts
+++ lib/graphql/mutations/selectAddress.handler.d.ts
@@ -1,3 +1,3 @@
 import type { ThunkActionDispatch } from "redux-thunk";
 import { CartShipmentsResponse } from "../../types/graphql";
-export declare function selectAddressHandler(res: CartShipmentsResponse, dispatch: ThunkActionDispatch<any>, params: unknown, oldStore: unknown): void;
+export declare function selectAddressHandler(res: CartShipmentsResponse, dispatch: ThunkActionDispatch<any>, params: unknown, oldStore: unknown, options?: any): void;
--- lib/graphql/mutations/updateCart.handler.d.ts
+++ lib/graphql/mutations/updateCart.handler.d.ts
@@ -1,3 +1,3 @@
 import type { ThunkActionDispatch } from "redux-thunk";
 import { UpdateCartDetailResponse } from "../../types/graphql";
-export declare function updateCartHandler(updateCart: UpdateCartDetailResponse, dispatch: ThunkActionDispatch<any>, params: any, oldStore: unknown): void;
+export declare function updateCartHandler(updateCart: UpdateCartDetailResponse, dispatch: ThunkActionDispatch<any>, params: any, oldStore: unknown, options?: any): void;
