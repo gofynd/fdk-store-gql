92ffdfebe08cf04bd804da6dfa96533164cb1641
Jigar Dafda <jigar.dafda@gmail.com>
2025-06-10 07:33:35 +0000

    [Auto Generated]

--- lib/store/utils/payment.d.ts
+++ lib/store/utils/payment.d.ts
@@ -3,6 +3,6 @@ export declare function getUpiIntentApps(paymentOptions: any): Promise<any>;
 export declare function verifyCardDetails(payload: any): Promise<void>;
 export declare function handleCODOrder(paymentOptions: any): Promise<void>;
 export declare function handleQrOrder(paymentOptions: object): Promise<any>;
-export declare function transactOrder(paymentOptions: any, state: any): Promise<any>;
+export declare function transactOrder(paymentOptions: any, state: any): Promise<void>;
 export declare function updateOrderInfo(currentPaymentGateway: any, orderInfo: any, paymentOptions: any): any;
 export declare function handleUpiOrder(paymentOptions: any): Promise<any>;
