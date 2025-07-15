772b227e16506d6bc48f7743b2a5f3d2a7defe9c
Jigar Dafda <jigar.dafda@gmail.com>
2025-07-15 12:50:52 +0000

    [Auto Generated]

--- lib/graphql/query/collection.handler.d.ts
+++ lib/graphql/query/collection.handler.d.ts
@@ -1,3 +1,3 @@
 import type { ThunkActionDispatch } from "redux-thunk";
 import { Collection } from "../../types/graphql";
-export declare function collectionHandler(collection: Collection, dispatch: ThunkActionDispatch<any>, params: unknown, oldStore: unknown): void;
+export declare function collectionHandler(collection: Collection, dispatch: ThunkActionDispatch<any>, params: unknown, oldStore: unknown, options?: any): void;
--- lib/graphql/query/productsPriceWithFulfillmentOption.handler.d.ts
+++ lib/graphql/query/productsPriceWithFulfillmentOption.handler.d.ts
@@ -1,3 +1,3 @@
 import { ProductSizePrice } from "../../types/graphql";
 import { ThunkActionDispatch } from "redux-thunk";
-export declare function productsPriceWithFulfillmentOptionHandler(productPrice: ProductSizePrice, dispatch: ThunkActionDispatch<any>, params: any, oldStore: unknown, error: object): void;
+export declare function productsPriceWithFulfillmentOptionHandler(productPrice: ProductSizePrice, dispatch: ThunkActionDispatch<any>, params: any, oldStore: unknown, options: any, error: object): void;
