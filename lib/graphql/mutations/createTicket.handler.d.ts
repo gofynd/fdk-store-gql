de4bbe1139b9d9113c37af7baef3faf718b72a4b
Jigar Dafda <jigar.dafda@gmail.com>
2026-03-26 11:55:28 +0000

    [Auto Generated]

---
+++ lib/graphql/mutations/createTicket.handler.d.ts
@@ -0,0 +1,3 @@
+import type { ThunkActionDispatch } from "redux-thunk";
+import { SupportTicket } from "../../types/graphql";
+export declare function createTicketHandler(createTicket: SupportTicket, dispatch: ThunkActionDispatch<any>, params: any, oldStore: unknown, options?: any): void;
